e26e1794d1e053d722865e719414681bcff145e6
ghost <belikesnab@gmail.com>
2026-03-16 09:02:06 -0400

    ios project init

---
+++ ios/PoochScan/PoochScan/PoochScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoochScanApp.swift
+//  PoochScan
+//
+//  Created by Samuel Brown on 3/16/26.
+//
+
+import SwiftUI
+
+@main
+struct PoochScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
