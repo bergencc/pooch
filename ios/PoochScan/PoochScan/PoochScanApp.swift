6692f7a9850f667674c7e6ba9d11a6c8b69f479d
ghost <belikesnab@gmail.com>
2026-03-17 05:40:01 -0400

    updated main

--- ios/PoochScan/PoochScan/PoochScanApp.swift
+++ ios/PoochScan/PoochScan/PoochScanApp.swift
@@ -6,7 +6,7 @@ struct PoochScanApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            AppRootView()
                 .environmentObject(authViewModel)
         }
     }
