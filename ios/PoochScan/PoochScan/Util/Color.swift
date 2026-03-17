942300ef7058b7ca3c384106c9a5935c16d4b80f
ghost <belikesnab@gmail.com>
2026-03-17 05:39:09 -0400

    updated color

--- ios/PoochScan/PoochScan/Util/Color.swift
+++ ios/PoochScan/PoochScan/Util/Color.swift
@@ -4,3 +4,8 @@ extension Color {
     static let brandOrange = Color(red: 0.97, green: 0.49, blue: 0.17)
     static let brandOrangeDark = Color(red: 0.87, green: 0.33, blue: 0.07)
 }
+extension ShapeStyle where Self == Color {
+    static var brandOrange: Color { Color.brandOrange }
+    static var brandOrangeDark: Color { Color.brandOrangeDark }
+}
+
